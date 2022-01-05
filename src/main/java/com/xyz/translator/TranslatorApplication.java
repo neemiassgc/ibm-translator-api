dbc6217b2cd65600bc62fb0477461e90a369e7d6
Neemias Santos <neemiassgc@outlook.com>
2022-01-05 11:47:08 -0300

    Add initial Spring Gradle project

---
+++ src/main/java/com/xyz/translator/TranslatorApplication.java
@@ -0,0 +1,12 @@
+package com.xyz.translator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslatorApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranslatorApplication.class, args);
+	}
+}
---
+++ src/test/java/com/xyz/translator/TranslatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xyz.translator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
