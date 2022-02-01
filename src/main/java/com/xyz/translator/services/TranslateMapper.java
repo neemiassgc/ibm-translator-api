dd841e8089955e5c79b601a751cb86692ed249ec
Neemias Santos <neemiassgc@outlook.com>
2022-02-01 17:10:55 -0300

    Add method mapLanguages to the TranslateMapper service class

--- src/main/java/com/xyz/translator/services/TranslateMapper.java
+++ src/main/java/com/xyz/translator/services/TranslateMapper.java
@@ -1,9 +1,30 @@
 package com.xyz.translator.services;
 
+import com.ibm.watson.language_translator.v3.model.Language;
+import com.ibm.watson.language_translator.v3.model.TranslationResult;
+import com.xyz.translator.dto.LanguageOptionOutput;
+import com.xyz.translator.dto.TranslateRequestInput;
+import com.xyz.translator.dto.TranslateResponseOutput;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class TranslateMapper {
 
+    @Autowired
+    private TranslateService translateService;
 
+    public List<LanguageOptionOutput> mapLanguages(List<Language> languages) {
+        return languages
+            .stream()
+            .filter(lang -> lang.isSupportedAsSource() && lang.isSupportedAsTarget())
+            .map(lang ->
+                new LanguageOptionOutput(
+                    lang.getLanguageName(), lang.getNativeLanguageName(), lang.getLanguage()
+                )
+            ).collect(Collectors.toList());
+    }
 }
