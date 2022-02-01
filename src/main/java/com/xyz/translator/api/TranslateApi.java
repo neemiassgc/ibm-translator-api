9d7a314fc2e653cee68a75c229e70127999cd455
Neemias Santos <neemiassgc@outlook.com>
2022-02-01 18:25:28 -0300

    Add handler exception method to the TranslateApi controller

--- src/main/java/com/xyz/translator/api/TranslateApi.java
+++ src/main/java/com/xyz/translator/api/TranslateApi.java
@@ -1,20 +1,27 @@
 package com.xyz.translator.api;
 
+import com.ibm.cloud.sdk.core.service.exception.BadRequestException;
+import com.ibm.cloud.sdk.core.service.exception.ServiceResponseException;
 import com.xyz.translator.dto.LanguageOptionOutput;
 import com.xyz.translator.dto.TranslateRequestInput;
 import com.xyz.translator.dto.TranslateResponseOutput;
+import com.xyz.translator.error.ErrorResponse;
 import com.xyz.translator.services.TranslateMapper;
 import com.xyz.translator.services.TranslateService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin("*")
-@Controller
+@RestController
 public class TranslateApi {
 
     @Autowired
@@ -24,7 +31,6 @@ public class TranslateApi {
     private TranslateMapper translateMapper;
 
     @GetMapping(path = "/languages", produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
     public ResponseEntity<List<LanguageOptionOutput>> getLanguageOptions() {
         return ResponseEntity.ok(translateMapper.mapLanguages(translateService.listLanguages()));
     }
@@ -34,8 +40,13 @@ public class TranslateApi {
         produces = MediaType.APPLICATION_JSON_VALUE,
         consumes = MediaType.APPLICATION_JSON_VALUE
     )
-    @ResponseBody
     public TranslateResponseOutput translate(@RequestBody final TranslateRequestInput translateRequestInput) {
         return translateMapper.mapTranslateResponse(translateRequestInput);
+    }
+
+    @ExceptionHandler(ServiceResponseException.class)
+    public ResponseEntity<ErrorResponse> handleException(final ServiceResponseException sre) {
+        final ErrorResponse errorResponse = new ErrorResponse(sre);
+        return ResponseEntity.status(errorResponse.getStatusCode()).body(errorResponse);
     }
 }
