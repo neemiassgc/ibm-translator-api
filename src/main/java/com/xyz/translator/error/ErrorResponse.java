881b7aa461bb1b65a5af949154d6d2a56555bf1a
Neemias Santos <neemiassgc@outlook.com>
2022-02-01 18:24:12 -0300

    Add class to build errors

---
+++ src/main/java/com/xyz/translator/error/ErrorResponse.java
@@ -0,0 +1,24 @@
+package com.xyz.translator.error;
+
+import com.ibm.cloud.sdk.core.service.exception.ServiceResponseException;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public final class ErrorResponse {
+
+    private String message;
+    private Throwable cause;
+    private int statusCode;
+
+    public ErrorResponse(final ServiceResponseException sre) {
+        this(sre.getMessage(), sre.getCause(), sre.getStatusCode());
+    }
+
+    private ErrorResponse(String message, Throwable cause, int statusCode) {
+        this.message = message;
+        this.cause = cause;
+        this.statusCode = statusCode;
+    }
+}
