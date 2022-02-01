56c5defd432a398be98bf79ea5ca4c1e9766ad18
Neemias Santos <neemiassgc@outlook.com>
2022-02-01 14:08:14 -0300

    Create a Bean for LanguageTranslator

--- src/main/java/com/xyz/translator/configuration/LanguageTranslatorConf.java
+++ src/main/java/com/xyz/translator/configuration/LanguageTranslatorConf.java
@@ -1,21 +1,21 @@
-package com.xyz.translator.composers;
+package com.xyz.translator.configuration;
 
 import com.ibm.cloud.sdk.core.security.IamAuthenticator;
 import com.ibm.watson.language_translator.v3.LanguageTranslator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
-import java.util.Objects;
-
-public final class SingletonLanguageTranslator {
+@Configuration
+public class LanguageTranslatorConf {
 
     private static final String SERVICE_URL = System.getenv("IBM_CLOUD_SERVICE_URL");
     private static final String API_KEY = System.getenv("IBM_CLOUD_APIKEY");
     private static final String VERSION = System.getenv("IBM_CLOUD_VERSION");
 
-    private static LanguageTranslator INSTANCE;
-
-    public static LanguageTranslator getInstance() {
-        if (!Objects.isNull(INSTANCE)) return INSTANCE;
-
+    @Bean
+    @Scope("singleton")
+    public LanguageTranslator languageTranslator() {
         final IamAuthenticator authenticator = new IamAuthenticator.Builder().apikey(API_KEY).build();
 
         LanguageTranslator languageTranslator = new LanguageTranslator(VERSION, authenticator);
