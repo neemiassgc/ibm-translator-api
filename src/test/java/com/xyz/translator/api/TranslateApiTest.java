ec4326a56bf5cd6535da44ca44d255495c3e6e67
Neemias Santos <neemiassgc@outlook.com>
2022-02-02 11:32:25 -0300

    Create a setup method to the TranslateApiTest test class

--- src/test/java/com/xyz/translator/api/TranslateApiTest.java
+++ src/test/java/com/xyz/translator/api/TranslateApiTest.java
@@ -1,4 +1,21 @@
 package com.xyz.translator.api;
 
+import com.xyz.translator.services.TranslateMapper;
+import com.xyz.translator.services.TranslateService;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.mockito.Mockito.mock;
+
 final class TranslateApiTest {
+
+    private TranslateApi translateApi;
+    private TranslateService translateServiceMock;
+    private TranslateMapper translateMapperMock;
+
+    @BeforeEach
+    void setup() {
+        this.translateServiceMock = mock(TranslateService.class);
+        this.translateMapperMock = mock(TranslateMapper.class);
+        this.translateApi = new TranslateApi(this.translateServiceMock, this.translateMapperMock);
+    }
 }
