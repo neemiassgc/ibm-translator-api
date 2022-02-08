38fec555bd5829a549b3c18f00401a995ab8f52f
Neemias Santos <neemiassgc@outlook.com>
2022-02-07 21:48:56 -0300

    Add a setup method to the TranslateMapperTest class

--- src/test/java/com/xyz/translator/services/TranslateMapperTest.java
+++ src/test/java/com/xyz/translator/services/TranslateMapperTest.java
@@ -1,7 +1,18 @@
 package com.xyz.translator.services;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mockito;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class TranslateMapperTest {
+final class TranslateMapperTest {
 
+    private TranslateService translateServiceMock;
+    private TranslateMapper translateMapperUnderTest;
+
+    @BeforeEach
+    void setup() {
+        this.translateServiceMock = Mockito.mock(TranslateService.class);
+        this.translateMapperUnderTest = new TranslateMapper(this.translateServiceMock);
+    }
 }
