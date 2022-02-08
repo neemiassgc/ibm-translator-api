932dad58c101431023f36a6712c3e248f50e2e5e
Neemias Santos <neemiassgc@outlook.com>
2022-02-07 22:57:14 -0300

    Add unit test mapTranslateResponseTest to the TranslateMapperTest unit test class

--- src/test/java/com/xyz/translator/services/TranslateMapperTest.java
+++ src/test/java/com/xyz/translator/services/TranslateMapperTest.java
@@ -1,7 +1,11 @@
 package com.xyz.translator.services;
 
 import com.ibm.watson.language_translator.v3.model.Language;
+import com.ibm.watson.language_translator.v3.model.Translation;
+import com.ibm.watson.language_translator.v3.model.TranslationResult;
 import com.xyz.translator.dto.LanguageOptionOutput;
+import com.xyz.translator.dto.TranslateRequestInput;
+import com.xyz.translator.dto.TranslateResponseOutput;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -9,7 +13,10 @@ import org.mockito.Mockito;
 import java.util.List;
 import java.util.Vector;
 
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 final class TranslateMapperTest {
 
@@ -63,5 +70,30 @@ final class TranslateMapperTest {
         assertThat(actualList).isNotNull();
         assertThat(actualList).allSatisfy(lang -> assertThat(lang).isNotNull());
         assertThat(actualList).hasSize(4);
+    }
+
+    @Test
+    void mapTranslateResponseTest() {
+        // given
+        final TranslateRequestInput requestInput = new TranslateRequestInput("Hello", "en", "fr");
+        final TranslationResult translationResult = new TranslationResult() {
+            @Override
+            public List<Translation> getTranslations() {
+                return List.of(new Translation());
+            }
+        };
+        Mockito.when(this.translateServiceMock.translate(any(String.class), any(String.class), any(String.class)))
+            .thenReturn(translationResult);
+
+        // when
+        final TranslateResponseOutput actual = this.translateMapperUnderTest.mapTranslateResponse(requestInput);
+
+        // then
+        assertThat(actual).isNotNull();
+
+        verify(this.translateServiceMock, times(1))
+            .translate(any(String.class), any(String.class), any(String.class));
+        verify(this.translateServiceMock, only())
+            .translate(any(String.class), any(String.class), any(String.class));
     }
 }
