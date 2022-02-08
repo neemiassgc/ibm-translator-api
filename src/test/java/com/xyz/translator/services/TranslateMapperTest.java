216d7165339372f9078d006b2903a4a247418d24
Neemias Santos <neemiassgc@outlook.com>
2022-02-07 22:08:47 -0300

    Add unit test mapLanguagesTest to the TranslateMapperTest unit test class

--- src/test/java/com/xyz/translator/services/TranslateMapperTest.java
+++ src/test/java/com/xyz/translator/services/TranslateMapperTest.java
@@ -1,9 +1,15 @@
 package com.xyz.translator.services;
 
+import com.ibm.watson.language_translator.v3.model.Language;
+import com.xyz.translator.dto.LanguageOptionOutput;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+import java.util.Vector;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 final class TranslateMapperTest {
 
@@ -14,5 +20,48 @@ final class TranslateMapperTest {
     void setup() {
         this.translateServiceMock = Mockito.mock(TranslateService.class);
         this.translateMapperUnderTest = new TranslateMapper(this.translateServiceMock);
+    }
+
+    @Test
+    void mapLanguagesTest() {
+        // given
+        final List<Language> languages = new Vector<>(4);
+        for (int i = 0; i < 4; i++) {
+            Language lang = new Language() {
+                @Override
+                public Boolean isSupportedAsSource() {
+                    return true;
+                }
+
+                @Override
+                public Boolean isSupportedAsTarget() {
+                    return true;
+                }
+
+                @Override
+                public String getLanguage() {
+                    return "en";
+                }
+
+                @Override
+                public String getLanguageName() {
+                    return "English";
+                }
+
+                @Override
+                public String getNativeLanguageName() {
+                    return "English";
+                }
+            };
+            languages.add(lang);
+        }
+
+        // when
+        final List<LanguageOptionOutput> actualList = this.translateMapperUnderTest.mapLanguages(languages);
+
+        // then
+        assertThat(actualList).isNotNull();
+        assertThat(actualList).allSatisfy(lang -> assertThat(lang).isNotNull());
+        assertThat(actualList).hasSize(4);
     }
 }
