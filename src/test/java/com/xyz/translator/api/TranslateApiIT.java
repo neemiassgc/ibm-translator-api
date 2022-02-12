390148000b503e2eee1aabed8b7a47baa29dcaaa
Neemias Santos <neemiassgc@outlook.com>
2022-02-12 07:39:03 -0300

    Create TranslateApiIT integration test class to test TranslateApi controller

---
+++ src/test/java/com/xyz/translator/api/TranslateApiIT.java
@@ -0,0 +1,18 @@
+package com.xyz.translator.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class TranslateApiIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+}
