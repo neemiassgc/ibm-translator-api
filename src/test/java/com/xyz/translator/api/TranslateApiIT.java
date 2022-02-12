40453373ce44509240ab7fc4d62fd3cab2728c0b
Neemias Santos <neemiassgc@outlook.com>
2022-02-12 09:09:39 -0300

    Add integration test shouldReturnLanguageOptions to the TranslateApiIT class

--- src/test/java/com/xyz/translator/api/TranslateApiIT.java
+++ src/test/java/com/xyz/translator/api/TranslateApiIT.java
@@ -1,10 +1,18 @@
 package com.xyz.translator.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
@@ -15,4 +23,16 @@ public class TranslateApiIT {
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @Test
+    void shouldReturnLanguageOptions() throws Exception {
+        final MockHttpServletResponse response = mockMvc.perform(get("/languages"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$").exists())
+            .andExpect(jsonPath("$").isArray())
+            .andExpect(jsonPath("$[9].languageName").value("German"))
+            .andExpect(jsonPath("$[9].nativeLanguageName").value("Deutsch"))
+            .andExpect(jsonPath("$[9].language").value("de")).andReturn().getResponse();
+    }
 }
