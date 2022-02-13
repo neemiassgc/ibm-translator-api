b3a3b0a7b0dc030730e7cb727401e0caf3719b77
Neemias Santos <neemiassgc@outlook.com>
2022-02-13 14:10:33 -0300

    Force the use of HTTPS by Spring Security

---
+++ src/main/java/com/xyz/translator/configuration/WebSecurityConfig.java
@@ -0,0 +1,15 @@
+package com.xyz.translator.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfig implements WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.requiresChannel().requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null).requiresSecure();
+    }
+}
